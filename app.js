b3d076c2a183ba59c6304329a67f3131b7b5bfe5
jbays <jjbaize@gmail.com>
2018-06-09 12:17:10 -0500

    Adding dependencies.  Refactoring vars.  Trying to get auth working

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const routes = require('./routes/index');
+const users = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,18 +20,58 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'this is a big, big secret',
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', routes);
+
+const Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+
+mongoose.connect('mongod://localhost/passport_local_mongoose_express4')
+
+app.use((req,res,next)=>{
+  let err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+})
+
+//handles development environment
+if ( app.get('env') === 'development' ) {
+  app.use((err,req,res,next)=>{
+    res.status(err.status || 500)
+    res.render('error', {
+      message: err.message,
+      error:err
+    })
+  })
+}
+
+//handles development environment
+if ( app.get('env') === 'development' ) {
+  app.use((err,req,res,next)=>{
+    res.status(err.status || 500)
+    res.render('error', {
+      message: err.message,
+      error:err
+    })
+  })
+}
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next)=>{
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
